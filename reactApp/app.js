da2123e824af03df378e551e05e87e24466f31bd
Mason <lantay@alumni.unc.edu>
2017-07-24 13:38:09 -0700

    eslint stuff from before, put in boiler plate from draft.js, set up app from react redux a bit

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,11 +1,31 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+import MyEditor from './draftJsCode';
+import { connect } from 'react-redux';
 
+const React = require('react');
+const ReactDOM = require('react-dom');
 /* This can check if your electron app can communicate with your backend */
 // fetch('http://localhost:3000')
 // .then(resp => resp.text())
 // .then(text => console.log(text))
 // .catch(err => {throw err})
+class App extends React.Component {
+  render() {
 
-ReactDOM.render(<p>React lives!</p>,
-   document.getElementById('root'));
+
+    return (
+      <p>React lives!</p> , // eslint-disable-line
+      <MyEditor />
+    )
+  }
+
+  const mapStateToProps = (state) => ({
+    //  someStateProp: /* state.something typically */
+  });
+
+  const mapDispatchToProps = (dispatch) => ({
+    //  someDispProp: /* some function that dispatches an action */
+  });
+
+
+  App = connect(mapStateToProps, mapDispatchToProps)(App);
+  export default App;
